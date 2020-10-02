3ee0e85d6696bc0b4d26c7690ae2754c02db68db
髙橋和晃 <kazuaki.flute@gmail.com>
2020-10-02 10:25:43 +0900

    implement input and output

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	var a, b int
+
+	fmt.Fscan(r, &a, &b)
+
+	fmt.Fprintln(w, a, b)
+
 	fmt.Println("Odd")
 }
